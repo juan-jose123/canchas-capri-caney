ffd9c8707cf7e72bf59baa62356f00f4edc46868
juan-jose123 <juanjoseparraramirez9@gmail.com>
2026-05-21 12:55:20 -0500

    Fix: Express 5 catch-all route compatibility

--- server/index.js
+++ server/index.js
@@ -265,8 +265,12 @@ io.on('connection', (socket) => {
 
 // Catch-all: serve frontend for any non-API route (SPA support)
 if (existsSync(distPath)) {
-  app.get('*', (req, res) => {
-    res.sendFile(join(distPath, 'index.html'));
+  app.use((req, res, next) => {
+    if (req.method === 'GET' && !req.path.startsWith('/api') && !req.path.startsWith('/socket.io')) {
+      res.sendFile(join(distPath, 'index.html'));
+    } else {
+      next();
+    }
   });
 }
 
